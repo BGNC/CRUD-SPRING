4002e1430fa9f986d4875b446d9854e72915aef7
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-03-12 23:16:01 +0300

    edit/update and delete page

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -11,5 +11,50 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String studentname;
+    private int fee;
+    private String course;
 
+    public Student(){
+
+    }
+    public Student(Long id,String studentname,int fee,String course){
+        super();
+        this.id=id;
+        this.studentname=studentname;
+        this.course=course;
+        this.fee=fee;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentname() {
+        return studentname;
+    }
+
+    public void setStudentname(String studentname) {
+        this.studentname = studentname;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
